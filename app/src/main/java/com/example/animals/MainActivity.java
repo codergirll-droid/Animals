53274978d487a0d67eeb6922cd1e40ff1b8aa193
codergirll-droid <oduncu981@gmail.com>
2021-10-07 15:10:02 +0300

    initial commit

--- app/src/main/java/com/example/animals/MainActivity.java
+++ app/src/main/java/com/example/animals/MainActivity.java
@@ -11,16 +11,25 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btnArrayAdapter;
+    Button btnCustomAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnArrayAdapter = (Button) findViewById(R.id.btnArrayAdapter);
+        btnCustomAdapter = (Button) findViewById(R.id.btnCustomAdapter);
         btnArrayAdapter.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(MainActivity.this, ArrayAdapterActivity.class);
+                startActivity(intent);
+
+            }
+        });btnCustomAdapter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, CustomAdapterActivity.class);
                 startActivity(intent);
 
             }
